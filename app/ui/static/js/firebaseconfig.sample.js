84d9062b7b1a17f34c318446b062099aeefaa730
Terry Ryan <tpryan@google.com>
2017-07-07 15:53:17 -0700

    Removing IO piece for now.

--- app/ui/static/js/firebaseconfig.sample.js
+++
@@ -1,7 +0,0 @@
-var firebaseConfig = {
-    apiKey: "[generate an api key]",
-    authDomain: "[your project].firebaseapp.com",
-    databaseURL: "https://[your project].firebaseio.com",
-    storageBucket: "[your project].appspot.com",
-    messagingSenderId: "[generate an message key]"
-};
