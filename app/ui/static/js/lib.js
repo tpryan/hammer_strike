0b57a52dddef06f28cd0b8ac63794597733f802a
Terry Ryan <tpryan@google.com>
2017-07-10 16:42:07 -0700

    Refactored a bunch of the js code.

---
+++ app/ui/static/js/lib.js
@@ -0,0 +1,56 @@
+
+
+function DISTRIBUTOR(hostname){
+
+    this.debug = false;
+    var apihostname = hostname;
+    this.timeout = 1000;
+    var apiprotocol = "http://"
+
+    var uri_report = "/distributor/report";
+    var uri_load = "/distributor";
+
+    var ajaxProxy = function(url, successHandler, errorHandler, timeout) {
+        timeout = typeof timeout !== 'undefined' ? timeout : this.timeout;
+        $.ajax({
+            url: url,
+            success: successHandler,
+            error: errorHandler,
+            timeout: timeout
+
+        });
+        if (this.debug){
+            console.log("Called: ", url);
+        }
+    };
+
+    var defaultErrorHandler = function(e){
+        console.log("Error in distributor api:", e);
+    };
+
+    var getReportURI = function(){
+        return apiprotocol + apihostname + uri_report;
+    };
+
+    var getLoadURI = function(){
+        return apiprotocol + apihostname + uri_load;
+    };
+
+    this.Report = function(token, successHandler){
+        ajaxProxy(getReportURI() +"?token=" + token, successHandler,
+                    defaultErrorHandler, this.timeout);
+    };
+
+    this.Load = function(token, count, successHandler){
+        console.log(getLoadURI() +"?token=" + token + "&n=" + count);
+        ajaxProxy(getLoadURI() +"?token=" + token + "&n=" + count,
+                    successHandler, defaultErrorHandler, this.timeout);
+    };
+
+}
+
+function GENERATOR(){
+    this.Get = function(){
+        return (Math.random().toString(36)+'00000000000000000').slice(2, 5+2)
+    };
+}
--- app/ui/static/js/main.js
+++ app/ui/static/js/main.js
@@ -1,91 +1,198 @@
+function HAMMER(max){
+    var positive = true;
+    var unit = max/500;
+    var stopped = false;
+    var min = max/20;
+    var htmlTweaked = false;
+
+
+    var setHTML = function(){
+        if (!htmlTweaked){
+            $("#loadcount").attr("min", min);
+            $("#loadcount").attr("max", max);
+            $("#loadcount").attr("step", unit);
+            htmlTweaked = true;
+        }
+    };
+
+
+    this.Move = function(){
+        setHTML();
+        var slider = document.querySelector("#loadcount");
+        var loadcount = parseInt(slider.value);
+
+        if (positive) {
+            loadcount += unit;
+            slider.value = loadcount;
+            if (loadcount >= max) {
+                positive = false;
+                slider.value = max;
+            }
+        } else {
+            loadcount -= unit;
+            slider.value = loadcount;
+            if (loadcount <= min) {
+                positive = true;
+                slider.value = min;
+            }
+        }
+
+        if (stopped) {
+            return;
+        }
+
+        if (loadcount > .8 * max) {
+            setTimeout(this.Move.bind(this), 1)
+        } else if (loadcount > .6 * max) {
+            setTimeout(this.Move.bind(this), 4)
+        } else if (loadcount > .4 * max) {
+            setTimeout(this.Move.bind(this), 6)
+        } else if (loadcount > .2 * max) {
+            setTimeout(this.Move.bind(this), 8)
+        } else {
+            setTimeout(this.Move.bind(this), 10)
+        }
+
+        var output = document.querySelector("#loadoutput");
+        output.value = slider.value;
+    };
+
+    this.Stop = function(){
+        stopped = true;
+    };
+
+    this.Restart = function(){
+        stopped = false;
+        this.Move();
+    };
+}
+
+function STATE(){
+    var bellrung = false;
+    var load_send = 0;
+    var interval;
+
+    this.IsBellRung = function(){
+        return bellrung;
+    };
+    this.SetBellRung = function(){
+        bellrung = true;;
+    };
+
+    this.SetLoadToSend = function(value){
+        load_send = value;
+    };
+
+    this.GetLoadToSend = function(){
+        return parseInt(load_send);
+    };
+
+    this.Reset = function(){
+        bellrung = false;
+        load_send = 0;
+        this.EndPoll();
+    };
+
+    this.StartPoll = function(pollHandler){
+        interval = setInterval(pollHandler, 100);
+    };
+
+    this.EndPoll = function(){
+        clearInterval(interval);
+    };
+
+}
+
+function RESULT(){
+
+    this.SetTotal = function(content){
+        $("#totalrequests span").html(content);
+    };
+
+    this.ClearHolder = function(){
+        $("#gaelist").html("");
+    };
+
+    this.ShowResults = function(report){
+        var seconds = (report.end - report.start) / 1000000000;
+        var qps = report.request_count / seconds;
+
+        if (seconds <= 0) {
+            seconds = 0;
+            qps = 0;
+        }
+        $("#totalinstances span").html(report.instance_count);
+        $("span.seconds").html(Math.round(seconds * 100) / 100);
+        $("span.qps").html(Math.round(qps * 100) / 100);
+
+        if (report.instances == null){
+            return;
+        }
+
+
+        for (var i = 0; i < report.instances.length; i++){
+            var gaeInstanceDiv = $("#"+ report.instances[i].name);
+
+            if (gaeInstanceDiv.length == 0){
+                var gaeInstanceDiv  = $("<div></div>");
+                gaeInstanceDiv.attr("id", report.instances[i].name);
+                gaeInstanceDiv.addClass("lifter");
+                gaeInstanceDiv.addClass(getRandomBinaryGender());
+                $("#gaelist").append(gaeInstanceDiv);
+            }
+
+            gaeInstanceDiv.html("<span>" + report.instances[i].requests + "</span>");
+        }
+    }
+
+    var getRandomBinaryGender = function (){
+        var rando = Math.random() * 10;
+        if (rando < 5 ){
+            return "male";
+        }
+        return "female";
+    };
+
+}
+
 var host = "https://hammer-strike-172817.appspot.com";
-var token = generateToken();
-var gaelist_endpoint = host + "/distributor/report";
-var load_creator = host + "/distributor";
+var hostname = "hammer-strike-172817.appspot.com";
+var max = 10000;
+var generator = new GENERATOR()
+var token = generator.Get();
+var distributor = new DISTRIBUTOR(hostname);
+var hammer = new HAMMER(max);
+var state = new STATE();
+var scoreboard = new RESULT();
 var bellstrike = new Audio('audio/bell.wav');
-var bellrung = false;
-var positive = true;
-var load_send = 0;
-var interval;
 var explain_visible = false;
 
-// config should be set in firebaseconfig.js
-// firebase.initializeApp(firebaseConfig);
-// var firebaseRef = firebase.database().ref('hammer-strike/strength');
-// var firebaseInit = false;
 
 document.addEventListener('DOMContentLoaded', function() {
-    sendFlush();
+    resetGame();
     getGAElist();
-    moveHammer();
-    document.querySelector("#flush").addEventListener("click", sendFlush);
+    hammer.Move();
+    document.querySelector("#flush").addEventListener("click", resetGame);
     document.querySelector("#load").addEventListener("click", handleStrike);
     document.querySelector("#load").focus();
     document.querySelector(".explain").addEventListener("click", toggle_explain);
-    // firebaseRef.on("value", function(snap){
-    //     if(firebaseInit){
-    //         sendLoad(snap.val());
-    //         document.querySelector("#loadcount").value = snap.val();
-    //         document.querySelector("#loadoutput").value = snap.val();
-    //     }
-    //     firebaseInit = true;
-    // });
+
 });
 
-function moveHammer(){
-    var unit = 20;
-    var slider = document.querySelector("#loadcount");
-    var loadcount = parseInt(slider.value);
 
-
-    if (positive){
-        loadcount += unit;
-        slider.value = loadcount;
-        if (loadcount >= 10000){
-            positive = false;
-            slider.value = 10000;
-        }
-    } else{
-        loadcount -= unit;
-        slider.value = loadcount;
-        if (loadcount <= 500){
-            positive = true;
-            slider.value = 500;
-        }
-    }
-
-    if (load_send > 0){
-        return;
-    }
-
-    if (loadcount > 8000){
-        setTimeout(moveHammer, 1)
-    } else if (loadcount > 6000){
-        setTimeout(moveHammer, 4)
-    } else if (loadcount > 4000){
-        setTimeout(moveHammer, 6)
-    } else if (loadcount > 2000){
-        setTimeout(moveHammer, 8)
-    } else {
-        setTimeout(moveHammer, 10)
-    }
-    var output = document.querySelector("#loadoutput");
-    output.value = slider.value;
-}
-
-function sendFlush(e){
-    document.querySelector("#flush").disabled = true;
-    document.querySelector("#load").disabled = false;
-    clearInterval(interval);
+function resetGame(e){
+    $("#flush").prop("disabled", true);
+    $("#load").prop("disabled", false);
     $(".alert").fadeOut('slow');
-    load_send = 0;
-    bellrung = false;
-    document.querySelector("#sentrequests span").innerHTML = 0;
-    token = generateToken();
-    document.querySelector("#gaelist").innerHTML="";
-    moveHammer();
-    document.querySelector("#gaelist").innerHTML="";
-    document.querySelector("#load").focus();
+    $("#sentrequests span").html(0);
+    $("#gaelist").html("");
+    $("#gaelist").html("");
+    $("#load").focus();
+
+    state.Reset();
+    token = generator.Get();
+    hammer.Restart();
+
     getGAElist();
 
     if (explain_visible){
@@ -100,122 +207,60 @@ function handleStrike(e){
 
 function sendLoad(count){
     console.log("Pressed");
-    interval = setInterval(getGAElist, 100);
-    document.querySelector("#flush").disabled = false;
-    document.querySelector("#load").disabled = true;
+    hammer.Stop();
+    state.StartPoll(getGAElist);
+    state.SetLoadToSend(count);
+    distributor.Load(token, count, handleInfo);
+
     $(".alert").fadeIn('slow');
     $("#gaelist-header").fadeIn('slow');
-    load_send = count;
+    $("#flush").prop("disabled", false);
+    $("#load").prop("disabled", true);
+    $("#sentrequests span").html(count);
 
     if (explain_visible){
         show_explanation();
     }
 
-    document.querySelector("#sentrequests span").innerHTML = load_send;
-    console.log(load_send);
-    console.log(load_creator + "?token=" + token + "&n=" +  load_send);
-    $.ajax({
-        url: load_creator + "?token=" + token + "&n=" +  load_send ,
-        success: log,
-        error: handleError
-
-    });
 }
-
 
 function getGAElist(){
     console.log("Polled.");
-    $.ajax({
-        url: gaelist_endpoint + "?token=" + token,
-        success: handleGAElist,
-        error: handleError
-
-    });
+    distributor.Report(token, handleGAElist);
 }
 
-
 function handleGAElist(e){
-    var loadcount = document.querySelector("#loadcount").value;
-    var holder = document.querySelector("#gaelist");
-    var totalHolder = document.querySelector("#totalrequests span");
-    var instanceHolder = document.querySelector("#totalinstances span");
-    var secondsHolder = document.querySelector("span.seconds");
-    var qpsHolder = document.querySelector("span.qps");
 
-    if (parseInt(load_send) == parseInt(e.request_count)){
-        totalHolder.innerHTML = "<br />All " + e.request_count;
+    if (state.GetLoadToSend() == parseInt(e.request_count)){
+        scoreboard.SetTotal("<br />All " + e.request_count);
+        state.EndPoll();
     } else {
-        totalHolder.innerHTML = e.request_count;
+        scoreboard.SetTotal(e.request_count)
     }
 
     if (e.request_count == 0){
-        holder.innerHTML = "";
+        scoreboard.ClearHolder();
     }
 
-    seconds = (e.end - e.start) / 1000000000;
-    qps = e.request_count / seconds;
+    scoreboard.ShowResults(e);
 
-    if (seconds <= 0) {
-        seconds = 0;
-        qps = 0;
-    }
-
-    instanceHolder.innerHTML = e.instance_count;
-    secondsHolder.innerHTML = Math.round(seconds * 100) / 100;
-    qpsHolder.innerHTML = Math.round(qps * 100) / 100;;
-
-    var ratio = e.request_count / 10000;
+    var ratio = e.request_count / max;
     var adj = 255 * ratio;
     document.querySelector("#striker").style.bottom = (90 + adj) + "px";
 
-    if ((adj > 250) && !bellrung ) {
+    if ((adj > 250) && !state.IsBellRung() ) {
         bellstrike.play();
-        bellrung = true;
+        state.SetBellRung();
     }
-
-    if (e.instances == null){
-        return;
-    }
-    for (var i = 0; i < e.instances.length; i++){
-        var div = document.getElementById(e.instances[i].name)
-
-        if (!div){
-
-            var cssClass = "female";
-            var rando = getRandomArbitrary(0, 10)
-            if (rando < 5 ){
-               cssClass = "male";
-            }
-
-            var div  = document.createElement("div");
-            div.classList.add("lifter");
-            div.classList.add(cssClass);
-            div.id = e.instances[i].name;
-            holder.appendChild(div);
-        }
-        div.innerHTML = "<span>" + e.instances[i].requests + "</span>";
-
-    }
-
-}
-
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
 }
 
 function handleError(e){
-    console.log(e);
+    console.log("Error:", e);
 }
 
-function log(e){
-    console.log(e);
+function handleInfo(e){
+    console.log("Info:", e);
 }
-
-
-function generateToken(){
- return (Math.random().toString(36)+'00000000000000000').slice(2, 5+2)
-}
-
 
 function toggle_explain(){
     var explain = $(".explain");
@@ -238,7 +283,7 @@ function toggle_explain(){
 
 function show_explanation(){
     $(".overlay-slider").fadeIn('slow');
-    if (load_send > 0){
+    if (state.GetLoadToSend > 0){
         $(".overlay-gaelist").fadeIn('slow');
         $(".overlay-alert").fadeIn('slow');
     }
@@ -247,4 +292,3 @@ function show_explanation(){
 function hide_explanation(){
     $(".overlay").fadeOut('slow');
 }
-
